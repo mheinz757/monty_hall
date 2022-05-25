83b339c5d25f89d54df32da6a7a9f3e60d75cdbb
Michael Heinz <heinz.38@osu.edu>
2022-05-25 12:22:37 -0700

    make asking for input clearer in monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     n = 10
-    print(f"There are {n} doors in front of you, labeled 0 through {n-1}. Behind one of them is a car, while the other {n-1} have goats behind them.")
+    print(f"There are {n} doors in front of you, labeled 0 through {n-1}.\nBehind one of them is a car, while the other {n-1} have goats behind them.")
 
     # create a set of all doors
     idxs = set(range(n))
@@ -14,20 +14,14 @@ if __name__ == "__main__":
 
     # get the user_idx
     # user_idx = random.randint(0, n-1)
-    print(f"Which door would you like to select? Please enter an integer between 0 and {n-1}. If you would like to exit, please type \"exit\".")
-    input1 = input()
-    if input1 == "exit":
-        print("Exiting...")
-        sys.exit()
-    user_idx = int(input1)
+    print(f"Which door would you like to select? Choose an integer between 0 and {n-1}.")
+    print(f"door> ", end = "")
+    user_idx = int(input())
     while user_idx not in range(n):
-        print(f"Error: door selected must be an integer between 0 and {n-1}.")
-        print(f"Which door would you like to select? Please enter an integer between 0 and {n-1}. If you would like to exit, please type \"exit\".")
-        input1 = input()
-        if input1 == "exit":
-            print("Exiting...")
-            sys.exit()
-        user_idx = int(input1)
+        print(f"Error: the door chosen must be an integer between 0 and {n-1}.")
+        print(f"Which door would you like to select? Choose an integer between 0 and {n-1}.")
+        print(f"door> ", end = "")
+        user_idx = int(input())
     
     # remove user_idx from idxs
     if user_idx in idxs:
@@ -59,24 +53,30 @@ if __name__ == "__main__":
 
     # Update user on remaining doors
     print("There are two doors remaining: ", end = "")
-    print(*idxs, sep = " and ", end = ". ")
-    print(f"One of these has a car behind it and the other has a goat behind it. You can either choose to stay with your original choice ({user_idx}) or switch to the other door.")
+    print(*idxs, sep = " and ", end = ".\n")
+    print(f"One of these has a car behind it and the other has a goat behind it.\nYou can either choose to stay with your original choice ({user_idx}) or switch to the other door.")
 
     # let the user select a strategy
-    print("Which strategy would you like to choose: \"switch\" or \"stay\"? If you would like to exit, please type \"exit\".")
-    input2 = input()
-    if input2 == "exit":
+    print("Which strategy would you like to choose?")
+    print("switch) Switch to the other door")
+    print("stay) Stay with original choice")
+    print("exit) Exit the program")
+    print("switch/stay/exit> ", end = "")
+    strategy = input().strip().lower()
+    if strategy == "exit":
         print("Exiting...")
         sys.exit()
-    strategy = input2
     while strategy not in ["switch", "stay"]:
         print("Error: strategy must be either \"switch\" or \"stay\".")
-        print("Which strategy would you like to choose: \"switch\" or \"stay\"? If you would like to exit, please type \"exit\".")
-        input2 = input()
-        if input2 == "exit":
+        print("Which strategy would you like to choose?")
+        print("switch) Switch to the other door")
+        print("stay) Stay with original choice")
+        print("exit) Exit the program")
+        print("switch/stay/exit> ", end = "")
+        strategy = input().strip().lower()
+        if strategy == "exit":
             print("Exiting...")
             sys.exit()
-        strategy = input2
 
     # decide if you win or lose based on the users strategy
     if strategy == "switch":
